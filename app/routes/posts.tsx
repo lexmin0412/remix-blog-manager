c2fb5817b35c8d3c5fa54340885d73ce868bfac0
lexmin0412 <zhangle_dev@outlook.com>
2022-03-09 01:44:59 +0800

    feat: 文章页添加展开收起按钮

--- app/routes/posts.tsx
+++ app/routes/posts.tsx
@@ -1,6 +1,7 @@
 import { Link, useLoaderData, Outlet } from "remix";
 import { getPosts } from '~/post'
 import postStyles from './../styles/posts/index.css'
+import { useState } from "react";
 
 export type Post = {
 	slug: string;
@@ -15,25 +16,38 @@ export const loader = async () => {
   return getPosts();
 };
 
-export default function Posts({request}) {
-	console.log('requestrequest', request);
-	
+export default function Posts() {
 	const posts = useLoaderData<Post[]>();
+	const [ collapsed, setCollapsed ] = useState(true);
 
+	const handleCollapseToggle = () => setCollapsed(!collapsed)
+	
 	return (
 		<main className="post-index">
-			<aside className="post-aside">
-			  <div className="home">
-					<Link to='/'>Lexmin Blog</Link>
+			{
+				collapsed ?
+				<div className="hamburger-btn"
+					onClick={handleCollapseToggle}
+				>
+					展开 》
 				</div>
-				<ul className="list-box">
-					{posts.map((post) => (
-						<li key={post.slug} className="list-item">
-							<Link to={`/posts/${post.slug}`}>{post.title}</Link>
-						</li>
-					))}
-				</ul>
-			</aside>
+				:
+					<aside className="post-aside">
+						<div className="home">
+							<Link to='/'>Lexmin Blog</Link>
+						</div>
+						<ul className="list-box">
+							{posts.map((post) => (
+								<li key={post.slug} className="list-item">
+									<Link to={`/posts/${post.slug}`}>{post.title}</Link>
+								</li>
+							))}
+						</ul>
+						<div className="collapse-btn"
+							onClick={handleCollapseToggle}
+						>《 折叠</div>
+					</aside>
+			}
 			<main className="posts-main">
 				<Outlet />
 			</main>
