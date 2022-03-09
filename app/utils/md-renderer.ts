cb60529ad7ace09358724124d08e324cb2e26d1f
lexmin0412 <zhangle_dev@outlook.com>
2022-03-09 22:52:05 +0800

    feat: 自定义marked转换后的样式

--- app/utils/md-renderer.ts
+++ app/utils/md-renderer.ts
@@ -27,9 +27,18 @@ export const transformMarkdown = (markdown: string) => {
       
       return `<pre class="bg-[#1f134e] my-4 px-4 py-4 rounded-xl"><code class="language-${infostring}">${code}</code></pre>`;
     },
-    // table(header: string, body) {
-    //   return `<table class="table-auto w-full">${header}${body}</table>`;
-    // }
+    table(header: string, body: string) {
+      console.log('header', header);
+      header = header.replace(/<tr>/g, '<div class="flex border-b py-2 border-b border-gray-600 text-sm">')
+        .replace(/<\/tr>/g, '<\/div>').replace(/<th>/g, '<div class="text-left font-bold flex-1">').replace(/<\/th>/g, '<\/div>');
+      console.log('替换后的header', header);
+      body = body.replace(/<tr>/g, '<div class="flex py-2 border-b border-gray-600">')
+        .replace(/<\/tr>/g, '<\/div>').replace(/<\/td>/g, '<\/div>').replace(/<td>/g, '<div class="text-left flex-1 text-sm">')
+      
+      console.log('body', body);
+      
+      return `<div class="mb-4">${header}${body}</div>`;
+    }
   };
   marked.use({renderer});
   const html = marked(markdown);
